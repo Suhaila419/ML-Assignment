3dc72a9dacffb2f735c171f69632002ebc82776f
Suhaila419 <163913554+Suhaila419@users.noreply.github.com>
2024-11-01 17:43:41 +0000

    Add files via upload

---
+++ train_validate_test.py
@@ -0,0 +1,34 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import matplotlib.image as pltImg
+import pandas as pd
+from sklearn.naive_bayes import GaussianNB
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+
+iris = pd.read_csv("iris_dataset/iris.csv")
+data = iris.drop("species", axis=1)  # input features ---> drop output feature
+label = iris["species"]  # target output
+iris.hist(bins=20,figsize=(20,15))
+plt.show()
+#data_train,data_test,label_train,label_test=train_test_split(data,label,test_size=0.3,random_state=0,shuffle=True)#shuffle --->sort data
+def train_validate_test_split(data, label, testRatio=0.3, valRatio=0.3):
+    # Calculate split sizes
+    test_size = int(len(data) * testRatio)
+    val_size = int(len(data) * valRatio)
+    train_size = len(data) - test_size - val_size
+
+    # Split the data and labels
+    data_train, label_train = data[:train_size], label[:train_size]
+    data_val, label_val = data[train_size:train_size + val_size], label[train_size:train_size + val_size]
+    data_test, label_test = data[train_size + val_size:], label[train_size + val_size:]
+
+    return data_train, data_val, data_test, label_train, label_val, label_test
+
+data_train, data_val, data_test, label_train, label_val, label_test = train_validate_test_split(data, label)
+print("Training Data:\n", data_train)# 60 row x 4 column (input)
+print("Validation Data:\n", data_val)# 45 row x 4 column (input)
+print("Test Data:\n", data_test)# 45 row x 4 column (input)
+print("Training label:\n", label_train)# 60 row x 4 column (output --> actual input)
+print("Validation label:\n", label_val)# 45 row x 4 column (output --> actual input)
+print("Test Label:\n", label_test)# 45 row x 4 column (output --> actual input)
