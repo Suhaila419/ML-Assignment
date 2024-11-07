cae18ac9a6805cecdce462ae874a83845f0b6c4d
Suhaila419 <163913554+Suhaila419@users.noreply.github.com>
2024-11-07 22:23:29 +0000

    Add files via upload

--- Loss.py
+++ Loss.py
@@ -1,27 +1,14 @@
 import numpy as np
-from matplotlib import pyplot as plt
-import pandas as pd
-from sklearn.naive_bayes import GaussianNB
-from sklearn.utils import shuffle
-from sklearn.linear_model import LogisticRegression
-from itertools import combinations
-from sklearn.datasets import load_iris
-from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
-
-# Load and shuffle the Iris dataset
-iris = pd.read_csv("iris_dataset/iris.csv")
-data = iris.drop("species", axis=1)   # Input features
-label = iris["species"]               # label --> output for actual input
+from sklearn.datasets import make_regression
+# make_regression ----> create a dataset for testing or illustrating linear regression models
+#  noise---> to make the data more realistic.
+data, label = make_regression(n_samples=1000, n_features=3, noise=1, random_state=0)
 
 data_train , data_test, label_train, label_test = train_test_split(data,label,test_size=0.3,random_state=0)# Shuffle data for balanced distribution across splits
 
-# Train a Gaussian Naive Bayes model
-GuassianNBModel = GaussianNB()
-GuassianNBModel.fit(data_train, label_train)
 
-x=data_train.values
-matrix_X_Train=np.array(x)
+matrix_X_Train=np.array(data_train)
 matrix_X_Test=np.array(data_test)
 # Create a column of ones with the same number of rows as matrix_X
 ones_column_Train = np.ones((matrix_X_Train.shape[0], 1))
@@ -43,13 +30,8 @@ inverse_square_matrix=np.linalg.inv(square_matrix)
 # (X_transpose * X) ^ inverse * X_transpose ---> 5 row x 5 column * 5 row x 105 column ---> 5 row x 105 column
 inv_Tran=inverse_square_matrix.dot(X_transpose)
 
-# Encode the class labels as numeric values
-label_encoder = LabelEncoder()
-label_encoded_test = label_encoder.fit_transform(label_test)
-label_encoded_train = label_encoder.fit_transform(label_train)
-
 # W=(((X^T)*X)^-1)*(X^T)* Y ----> 5 row x 105 column * 105 row x 1 column --> 5 row x 1 column
-W=inv_Tran.dot(label_encoded_train)
+W=inv_Tran.dot(label_train)
 
 # prediction to compute error
 predict_y=X_Test.dot(W)
@@ -57,5 +39,4 @@ predict_y=X_Test.dot(W)
 def compute_loss(predicted_y, y): 
     loss=0.5*(np.mean((y - predicted_y) ** 2))
     return loss
-print(compute_loss(predict_y,label_encoded_test))
-
+print(compute_loss(predict_y,label_test))
